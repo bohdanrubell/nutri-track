858bd6b37862a1e75ffadf1578361ca5ceffaee5
Bohdan Rubel <bohdan.rubel@hneu.net>
2025-03-13 23:05:08 +0200

    created product controller and added an endpoint for testing.

---
+++ API/Controllers/ProductNutritionController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NutriTrack.Data;
+using NutriTrack.Entities;
+
+namespace NutriTrack.Controllers;
+
+public class ProductNutritionController : BaseApiController
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductNutritionController(ApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{id}", Name = "GetProductNutrition")]
+    public async Task<ActionResult<ProductNutrition>> GetProductNutrition(int id)
+    {
+        var productNutrition = await _context.ProductNutritions.FindAsync(id);
+
+        if (productNutrition == null) return NotFound();
+
+        return productNutrition;
+    }
+}
