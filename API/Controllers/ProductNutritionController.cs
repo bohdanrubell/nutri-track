78eb13dfc6a7249906c700fe7224d045e9cfe9e7
Bohdan Rubel <bohdan.rubel@hneu.net>
2025-03-14 11:07:51 +0200

    added en endpoint to get all product categories

--- API/Controllers/ProductNutritionController.cs
+++ API/Controllers/ProductNutritionController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NutriTrack.Data;
+using NutriTrack.DTO;
 using NutriTrack.Entities;
 
 namespace NutriTrack.Controllers;
@@ -17,10 +19,25 @@ public class ProductNutritionController : BaseApiController
     [HttpGet("{id}", Name = "GetProductNutrition")]
     public async Task<ActionResult<ProductNutrition>> GetProductNutrition(int id)
     {
-        var productNutrition = await _context.ProductNutritions.FindAsync(id);
+        var productNutrition = await _context.ProductNutritions
+            .Where(n => n.Id == id)
+            .FirstOrDefaultAsync();
 
         if (productNutrition == null) return NotFound();
 
         return productNutrition;
     }
+
+    [HttpGet("categories")]
+    public async Task<ActionResult<List<ProductNutritionCategoryResponse>>> GetProductNutritionCategories()
+    {
+        return await _context.ProductNutritionCategories
+            .Select(p => new ProductNutritionCategoryResponse
+            {
+                Id = p.Id,
+                Name = p.Name
+            })
+            .ToListAsync();
+    }
+    
 }
---
+++ API/DTO/ProductNutritionCategoryResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace NutriTrack.DTO;
+
+public class ProductNutritionCategoryResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
