d7277c251b97d363865835458ffc5d536021600d
Bohdan Rubel <bohdan.rubel@hneu.net>
2025-03-26 23:17:26 +0200

    feat(ui): add provider/router provider and toast container for main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {Provider} from "react-redux";
+import {store} from "./app/store/store.ts";
+import {ToastContainer} from "react-toastify";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./app/router/Routes.tsx";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Provider store={store}>
+          <ToastContainer position='bottom-right' theme='dark' />
+          <RouterProvider router={router} />
+      </Provider>
   </StrictMode>,
 )
