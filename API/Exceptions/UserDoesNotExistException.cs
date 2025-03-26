2e8f4c77be67e3654ad262902c4aa0e0bd109a80
Bohdan Rubel <bohdan.rubel@hneu.net>
2025-03-26 22:36:45 +0200

    feat(api): added custom exceptions

---
+++ API/Exceptions/UserDoesNotExistException.cs
@@ -0,0 +1,10 @@
+﻿namespace NutriTrack.Exceptions;
+
+public class UserDoesNotExistException : ApplicationException
+{
+    public UserDoesNotExistException(string userId)
+    : base($"Користувача з ID: {userId} не було знайдено!")
+    {
+        
+    }
+}
---
+++ API/Exceptions/UserIsNotAuthorizedException.cs
@@ -0,0 +1,9 @@
+﻿namespace NutriTrack.Exceptions;
+
+public class UserIsNotAuthorizedException : ApplicationException
+{
+    public UserIsNotAuthorizedException() 
+        : base("Користувач не авторизований!") 
+    {
+    }
+}
