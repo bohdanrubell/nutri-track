ddf8cfe9b96515c44ab0da25da708fe0277c8686
Bohdan Rubel <bohdan.rubel@hneu.net>
2025-03-29 08:09:25 +0200

    feat(api): add migration

--- API/Data/Migrations/20250328201751_UpdatedTypesOfCPFC.Designer.cs
+++ API/Data/Migrations/20250328201751_UpdatedTypesOfCPFC.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NutriTrack.Data;
 
@@ -11,9 +12,11 @@ using NutriTrack.Data;
 namespace NutriTrack.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250328201751_UpdatedTypesOfCPFC")]
+    partial class UpdatedTypesOfCPFC
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
