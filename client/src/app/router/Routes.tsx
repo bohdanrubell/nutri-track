52bfff6a06ccb5f8df1592ac9ef22833ce670e98
Bohdan Rubel <bohdan.rubel@hneu.net>
2025-03-29 09:11:37 +0200

    feat(ui): add new routes

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -5,6 +5,8 @@ import Register from "../../features/account/Register.tsx";
 import Profile from "../../features/account/Profile.tsx";
 import ProductNutrition from "../../features/productNutrition/ProductNutrition.tsx";
 import Diary from "../../features/diary/Diary.tsx";
+import ProductNutritionDetails from "../../features/productNutrition/ProductNutritionDetails.tsx";
+import PeriodStatisticsView from "../../features/statistics/PeriodStatisticsView.tsx";
 
 export const router = createBrowserRouter(([
     {
@@ -13,6 +15,8 @@ export const router = createBrowserRouter(([
         children: [
             {path: '/login', element: <Login/>},
             {path: '/productNutrition', element: <ProductNutrition/>},
+            {path: '/productNutrition/:id', element: <ProductNutritionDetails/>},
+            {path: '/statistics', element: <PeriodStatisticsView/>},
             {path: '/register', element: <Register/>},
             {path: '/profile', element: < Profile/>},
             {path: '/diary', element: < Diary/>},
